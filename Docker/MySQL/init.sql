dbfda79c3f9c1fb2280a9cc098c03d2e4130411f
Taka <121748062+TakahitoUchino@users.noreply.github.com>
2023-07-14 23:44:29 +0900

    alarm機能のテーブルとCRUD処理を実装

--- Docker/MySQL/init.sql
+++ Docker/MySQL/init.sql
@@ -29,6 +29,12 @@ CREATE TABLE messages (
     created_at timestamp not null default current_timestamp
 );
 
+CREATE TABLE alarms  (
+    id serial PRIMARY KEY,
+    uid varchar(255) REFERENCES users(id) UNIQUE NOT NULL,
+    alarm time NOT NULL
+);
+
 INSERT INTO users(uid, user_name, email, password)VALUES('970af84c-dd40-47ff-af23-282b72b7cca8','テスト','test@gmail.com','37268335dd6931045bdcdf92623ff819a64244b53d0e746d438797349d4da578');
 INSERT INTO channels(id, uid, name, abstract)VALUES(1, '970af84c-dd40-47ff-af23-282b72b7cca8','ぼっち部屋','テストさんの孤独な部屋です');
 INSERT INTO messages(id, uid, cid, message)VALUES(1, '970af84c-dd40-47ff-af23-282b72b7cca8', '1', '誰かかまってください、、')
